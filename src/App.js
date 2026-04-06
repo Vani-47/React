5c232da641d6f989e03d819b1d8f34dd463f9ba7
Vanisri <vanisrigangula@gmail.com>
2026-04-06 13:21:07 +0530

    added fx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './Components/mainScreen/Main';
-import Login from './Components/Login/Login';
 import ForgotPassword from './Components/Login/ForgotPassword';
+import Login from './Components/Login/Login';
 import Login from './Components/Login/Login';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/login" element={<Login1 />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/main" element={<Main />} />
           <Route path="/forgot" element={<ForgotPassword />} />
 
